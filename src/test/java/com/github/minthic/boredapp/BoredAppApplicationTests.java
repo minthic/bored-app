27da4ec177b916605757b3faac71b863ecdce352
gor <devmint@proton.me>
2022-08-25 00:05:44 +0400

    Add MySQL DB dependencies and app properties

--- src/test/java/com/github/minthic/boredapp/BoredAppApplicationTests.java
+++ src/test/java/com/github/minthic/boredapp/BoredAppApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BoredAppApplicationTests {
+class BoredAppApplicationTests
+{
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads()
+    {
+    }
 
 }
